e4ae4bb3b6055774c492d12246c7ad02147e5f9f
Oona Carbonie <carbonie.oona@outlook.com>
2019-05-01 10:30:53 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("Hello world!  ");
+
+        System.out.print ("Today is May 1st  ");
+
+        System.out.print("My name is michael  ");
+
+
+
+
+
+
+
+
+
+     }
+}
